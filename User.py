023485b230a12fd0f42411a4bb7b30c906d9450a
sivert-svanes <siverss@uia.no>
2026-03-09 16:50:10 +0100

    dev: implemented user constructor with KDA

---
+++ User.py
@@ -0,0 +1,16 @@
+﻿from App import Character
+
+
+class User:
+    def __init__ (self, character: Character, kills: int = 0, deaths: int = 0, assists: int = 0):
+        if not character:
+            raise ValueError("Character cannot be None")
+
+        self.character = character
+        self.kills = kills
+        self.deaths = deaths
+        self.assists = assists
+
+
+    def character_score_calculator(self):
+        return
