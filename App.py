74ef421925f71cf87ce0d16b20a58def1975da0c
sivert-svanes <siverss@uia.no>
2026-03-28 16:09:22 +0100

    dev: small patch update

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from random import Random, randint, choice
 from typing import Union, Tuple, Dict, Any
 
 class CharacterType(Enum):
@@ -19,7 +20,6 @@ class ItemType(Enum):
     GUN = 4
     SUSTAIN = 6
 
-
 class StatKey(Enum):
     HEALTH = 0
     SPIRITPOWER = 1
@@ -34,6 +34,16 @@ class StatKey(Enum):
 class ItemMode(Enum):
     ADD = 1
     MULTIPLY = 2
+
+class Teams(Enum):
+    ARCH_MOTHER = 1
+    HIDDEN_KING = 2
+
+class Choices(Enum):
+    calculate_item_to_buy = 1
+    check_teams = 2
+    next_phase = 3
+    exit = 4
 
 Effect = Union[float, Tuple[ItemMode, float], Dict[str, Union[str, float]]]
 class Item:
@@ -120,6 +130,11 @@ class Character:
         for key, value in self.statWeights.items():
             print(key.name,": ", value)
 
+    def print_type_weights(self):
+        print(self.name, "Type weights:")
+        for key, value in self.typeWeights.items():
+            print(key.name,": ", value)
+
 def relative_stat_increase(character: Character, item: Item):
     total_percent = 0.0
     for key_idx, (mode, val) in item.stats.items():
@@ -164,9 +179,191 @@ def calculate_item_to_buy(character: Character, possible_items: list[Item], debu
                 f"| compound percent stat increase = {increase:.2f}% | weighted score = {weighted_increase:.3f} |")
 
     return best_item[0]
+class User:
+    time = 0.0
+    Arch_Mother: set["User"] = set()
+    Hidden_King: set["User"] = set()
+
+    def __init__(self, character: Character, kills: int = 0, deaths: int = 0, assists: int = 0):
+        if not character:
+            raise ValueError("Character cannot be None")
+
+        self.character = character
+        self.kills = kills
+        self.deaths = deaths
+        self.assists = assists
+    def add_to_team(self, team: Teams, debug=False) -> None:
+        if team == Teams.ARCH_MOTHER:
+            if len(User.Arch_Mother) >= 5:
+                if debug:
+                    print("Arch Mother team is full. Cannot add more members.")
+                return
+        elif team == Teams.HIDDEN_KING:
+            if len(User.Hidden_King) >= 5:
+                if debug:
+                    print("Hidden King team is full. Cannot add more members.")
+                return
+        if self not in User.Arch_Mother and self not in User.Hidden_King:
+            if team == Teams.ARCH_MOTHER:
+                User.Arch_Mother.add(self)
+                if debug:
+                    print("Added to team Arch Mother:", self.character.name)
+            elif team == Teams.HIDDEN_KING:
+                User.Hidden_King.add(self)
+                if debug:
+                    print("Added to team Hidden King:", self.character.name)
+            else:
+                raise ValueError("Invalid team name. Must be 'Arch Mother' or 'Hidden King'.")
+        else:
+            print(f"{self.character.name} is already in a team. Cannot add to another team OR be added again.")
+    def check_team_membership(self) -> Teams:
+        if self in User.Arch_Mother:
+            return Teams.ARCH_MOTHER
+        elif self in User.Hidden_King:
+            return Teams.HIDDEN_KING
+        else:
+            raise ValueError("Not part off any team.")
+    def add_weight(self, itemType, updated_value, debug=False) -> None:
+        old = self.character.typeWeights.get(itemType)
+        print(f"Old weight for {itemType}: {old}")
+        if old is None:
+            print(f"[WARN] Missing key in typeWeights for {itemType} -> creating with 0.0")
+            old = 0.0
+        self.character.typeWeights[itemType] = old + updated_value
+        if debug:
+            print(f"Updated {itemType}: {old} -> {self.character.typeWeights[itemType]}")
+
+    def is_fed_calc(self, debug=False) -> None:
+        selfkda = (self.kills + (self.assists / 2)) / max(1, self.deaths)
+        team_check = self.check_team_membership()
+        team_variable = User.Arch_Mother if team_check == Teams.ARCH_MOTHER else User.Hidden_King
+        teammates = [user for user in team_variable if user is not self]
+        if len(teammates) == 0:
+            raise ValueError(f"No teammates to compare for {self.character.name}.")
+
+        total_kda = 0
+        team_avg_kda = 0
+        for user in teammates:
+            user_kda = (user.kills + (user.assists / 2)) / max(1, user.deaths)
+            total_kda += user_kda
+            team_avg_kda = total_kda / len(teammates)
+
+        if debug:
+            print(f"Total KDA / All teammate KDA added upp: {total_kda:.2f}")
+            print(f"Team Average KDA: {team_avg_kda:.2f}")
+            print(f"{self.character.name}'s KDA: {selfkda:.2f}")
+            print(f"Before Type Weights: {self.character.typeWeights}")
+
+        ctype = self.character.characterType
+
+
+        if selfkda > team_avg_kda:
+            if debug:
+                print("got past the selfkda > team_avg_kda check")
+            if ctype == CharacterType.SUPPORT:
+                if debug:
+                    print(f"{self.character.name} is a support, so they get more sustain items.")
+                self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+            elif ctype == CharacterType.TANK:
+                if debug:
+                    print(f"{self.character.name} is a tank, so they get more sustain and gun items.")
+                self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+                self.add_weight(ItemType.GUN, 0.4, debug=debug)
+            elif ctype == CharacterType.DPS:
+                if debug:
+                    print(f"{self.character.name} is a DPS, so they get more gun and spirit items.")
+                self.add_weight(ItemType.GUN, 0.4, debug=debug)
+
+            print(
+                f"{self.character.name} is fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
+        elif selfkda < team_avg_kda:
+            self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+            print(
+                f"{self.character.name} is not fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
+        if debug:
+            print(f"After Type Weights: {self.character.typeWeights}")
+
+    def menu(self):
+        self.character.print_stats()
+        print(f"Current Souls: {self.character.currentSouls}")
+        print("\n" + "-" * 50 + "\n")
+        if self.time == 0.0:
+            print("Welcome to the Item Recommendation System!")
+        print("Please select an option:")
+        print(f"1. Calculate optimal item for {self.character.name} and buy it")
+        print("2. Check team stats")
+        print("3. Next phase")
+        print("4. Exit")
+
+    def assign_stats_to_teams(self) -> None:
+        self.character.currentSouls += choice([800, 1600])
+        for user in User.Arch_Mother:
+            user.kills += randint(0, 5)
+            user.deaths += randint(0, 3)
+            user.assists += randint(0, 3)
+        for user in User.Hidden_King:
+            user.kills += randint(0, 5)
+            user.deaths += randint(0, 3)
+            user.assists += randint(0, 3)
+    def simulate_game(self):
+        print("Simulating game for", self.character.name)
+        while self.time <= 50.0:
+            self.divider()
+            self.menu()
+
+            if self.time > 0.0:
+                self.is_fed_calc(debug=True)
+
+            choice_user = input("Enter your choice: (1 - 4) ")
+            try:
+                choice_enum = Choices(int(choice_user))
+            except ValueError:
+                print("Was not able to parse choice. Please enter a number between 1 and 4.")
+                return
+
+            if choice_enum == Choices.calculate_item_to_buy:
+                optimal_item = calculate_item_to_buy(self.character, list(items.values()), debug=True)
+                if optimal_item:
+                    print(f"Optimal item to buy: {optimal_item.name}")
+                    self.character.buyitem(optimal_item)
+                else:
+                    print("No optimal item found or not enough souls to buy any item.")
+            elif choice_enum == Choices.check_teams:
+                User.check_teams()
+            elif choice_enum == Choices.next_phase:
+                self.assign_stats_to_teams()
+                print("Moving to next phase...")
+                self.time += 10
+                if self.time > 50.0:
+                    print("Reached end of simulation time.")
+                    break
+            elif choice_enum == Choices.exit:
+                print("Exiting the simulation. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please enter a number between 1 and 4.")
+
+    @classmethod
+    def check_teams(cls) -> None:
+        print("Arch Mother Supporters:")
+        for user in cls.Arch_Mother:
+            print(f"  {user.character.name} - Kills: {user.kills}, Assists: {user.assists}, Deaths: {user.deaths}")
+
+        print("Hidden King Supporters:")
+        for user in cls.Hidden_King:
+            print(f"  {user.character.name} - Kills: {user.kills}, Assists: {user.assists}, Deaths: {user.deaths}")
+
+    def divider(self):
+        print("\n" + "-" * 200 + "\n")
+        print(f"\n--- Minute {self.time} ---")
 
 if __name__ == '__main__':
 
+
+
+# TODO legg til slik at det bare kan være 1 av hver karakter, ikke duplikater
+# TODO Få initialiseringen av objekter inn i en egen fil, og importer det inn i App.py, for å gjøre det mer oversiktlig
+#region character setup
     infernus = Character(
         name='Infernus',
         characterType = CharacterType.DPS,
@@ -220,7 +417,7 @@ if __name__ == '__main__':
         },
         typeWeights={
             ItemType.SUSTAIN: 1.05,
-            ItemType.GUN: 1.05,
+            ItemType.GUN: 1.10,
             ItemType.SPIRIT: 1.45
         }
     )
@@ -253,29 +450,209 @@ if __name__ == '__main__':
             ItemType.SPIRIT: 1.25
         }
     )
-    from User import User, Teams
+
+    mokrill = Character(
+        name='Mo & Krill',
+        characterType=CharacterType.TANK,
+        currentSouls=2400,
+        stats={
+            StatKey.HEALTH:940,
+            StatKey.SPIRITPOWER:0,
+            StatKey.STAMINA:3,
+            StatKey.BULLETDAMAGE:2.82,
+            StatKey.AMMO:20,
+            StatKey.BPS:5.29,
+            StatKey.HEALTH_REGEN:1.0,
+            StatKey.MOVE_SPEED:8.0,
+            StatKey.SPIRIT_LIFESTEAL:0.0,
+        },
+        statWeights={
+            StatKey.BULLETDAMAGE:0.3,
+            StatKey.HEALTH:2.0,
+            StatKey.SPIRITPOWER:1.2,
+            StatKey.STAMINA:1.1,
+            StatKey.AMMO:0.2,
+        },
+        typeWeights={
+            ItemType.SUSTAIN:1.65,
+            ItemType.GUN:1.05,
+            ItemType.SPIRIT:1.25 }
+    )
+
+    mirage = Character(
+        name='Mirage',
+        characterType=CharacterType.DPS,
+        currentSouls=2400,
+        stats={
+            StatKey.HEALTH: 740,
+            StatKey.SPIRITPOWER: 0,
+            StatKey.STAMINA: 3,
+            StatKey.BULLETDAMAGE: 14.8,
+            StatKey.AMMO: 16,
+            StatKey.BPS: 2.72,
+            StatKey.HEALTH_REGEN: 2.0,
+            StatKey.MOVE_SPEED: 7.0,
+            StatKey.SPIRIT_LIFESTEAL: 0.0,
+        },
+        statWeights={
+            StatKey.BULLETDAMAGE:2.3,
+            StatKey.HEALTH:1.0,
+            StatKey.SPIRITPOWER:2.2,
+            StatKey.STAMINA:1.3,
+            StatKey.AMMO:0.2,
+        },
+        typeWeights={
+            ItemType.SUSTAIN:1.05,
+            ItemType.GUN:1.55,
+            ItemType.SPIRIT:1.35 }
+    )
+
+    shiv = Character(
+        name='Shiv',
+        characterType=CharacterType.DPS,
+        currentSouls=2400,
+        stats={
+            StatKey.HEALTH: 840,
+            StatKey.SPIRITPOWER: 0,
+            StatKey.STAMINA: 3,
+            StatKey.BULLETDAMAGE: 4.8,
+            StatKey.AMMO: 10,
+            StatKey.BPS: 1.81,
+            StatKey.HEALTH_REGEN: 2.0,
+            StatKey.MOVE_SPEED: 6.5,
+            StatKey.SPIRIT_LIFESTEAL: 0.0,
+        },
+        statWeights={
+            StatKey.BULLETDAMAGE:0.3,
+            StatKey.HEALTH:1.2,
+            StatKey.SPIRITPOWER:2.2,
+            StatKey.STAMINA:1.3,
+            StatKey.AMMO:0.2,
+        },
+        typeWeights={
+            ItemType.SUSTAIN:1.25,
+            ItemType.GUN:1.00,
+            ItemType.SPIRIT:1.65 }
+    )
+
+    paige = Character(
+        name='Paige',
+        characterType=CharacterType.SUPPORT,
+        currentSouls=2400,
+        stats={
+            StatKey.HEALTH: 690,
+            StatKey.SPIRITPOWER: 0,
+            StatKey.STAMINA: 2,
+            StatKey.BULLETDAMAGE: 35.52,
+            StatKey.AMMO: 14,
+            StatKey.BPS: 1.67,
+            StatKey.HEALTH_REGEN: 2.0,
+            StatKey.MOVE_SPEED: 6.9,
+            StatKey.SPIRIT_LIFESTEAL: 0.0,
+        },
+        statWeights={
+            StatKey.BULLETDAMAGE:0.3,
+            StatKey.HEALTH:2.2,
+            StatKey.SPIRITPOWER:1.2,
+            StatKey.STAMINA:1.5,
+            StatKey.AMMO:0.2,
+        },
+        typeWeights={
+            ItemType.SUSTAIN:1.65,
+            ItemType.GUN:1.00,
+            ItemType.SPIRIT:1.15 }
+    )
+
+    abrams = Character(
+        name='Abrams',
+        characterType=CharacterType.DPS,
+        currentSouls=2400,
+        stats={
+            StatKey.HEALTH: 810,
+            StatKey.SPIRITPOWER: 0,
+            StatKey.STAMINA: 3,
+            StatKey.BULLETDAMAGE: 3.6,
+            StatKey.AMMO: 9,
+            StatKey.BPS: 1.59,
+            StatKey.HEALTH_REGEN: 1.5,
+            StatKey.MOVE_SPEED: 6.4,
+            StatKey.SPIRIT_LIFESTEAL: 0.0,
+        },
+        statWeights={
+            StatKey.BULLETDAMAGE:1.3,
+            StatKey.HEALTH:2.2,
+            StatKey.SPIRITPOWER:1.2,
+            StatKey.STAMINA:1.5,
+            StatKey.AMMO:1.2,
+        },
+        typeWeights={
+            ItemType.SUSTAIN:1.65,
+            ItemType.GUN:1.10,
+            ItemType.SPIRIT:1.00 }
+    )
+
+#endregion
+#region user setup
+
 
     sivert = User(
         character=pocket,
-        kills=8,
-        deaths=1,
-        assists=5
+        kills=0,
+        deaths=0,
+        assists=0
     )
 
     tobias = User(
         character=infernus,
         kills=0,
-        deaths=10,
-        assists=4
+        deaths=0,
+        assists=0
     )
 
     iver = User(
         character=dynamo,
         kills=0,
-        deaths=20,
-        assists=4
+        deaths=0,
+        assists=0
     )
 
+    aragorn = User(
+        character=mokrill,
+        kills=0,
+        deaths=0,
+        assists=0
+    )
+
+    gimli = User(
+        character=shiv,
+        kills=0,
+        deaths=0,
+        assists=0
+    )
+
+    gandalf = User(
+        character=abrams,
+        kills=0,
+        deaths=0,
+        assists=0
+    )
+
+    legolas = User(
+        character=paige,
+        kills=0,
+        deaths=0,
+        assists=0
+    )
+
+    frodo = User(
+        character=mirage,
+        kills=0,
+        deaths=0,
+        assists=0
+    )
+
+#endregion
+#region item setup
     items = {
         'Titanic_mag': Item(
             name='Titanic Mag',
@@ -337,21 +714,25 @@ if __name__ == '__main__':
             }
         )
     }
+#endregion
     """""
     infernus.print_stats()
     print("")
 
     print("Optimal first item:", calculate_item_to_buy(infernus, list(items.values()), debug=True).name)
     infernus.buyitem(calculate_item_to_buy(infernus, list(items.values())))
-    for it in infernus.currentItems:
-        print(it.name)
+    for item in infernus.currentItems:
+        print(item.name)
 
     print("Optimal second item:", calculate_item_to_buy(infernus, list(items.values()), debug=True).name)
     """
 
     #Check User Class Functionality
-    sivert.add_to_team(Teams.ARCH_MOTHER)
-    tobias.add_to_team(Teams.HIDDEN_KING)
-    iver.add_to_team(Teams.ARCH_MOTHER)
-    User.check_team_characters(Teams.ARCH_MOTHER)
-    User.check_team_characters(Teams.HIDDEN_KING)
+    sivert.add_to_team(Teams.ARCH_MOTHER), iver.add_to_team(Teams.ARCH_MOTHER), legolas.add_to_team(Teams.ARCH_MOTHER), aragorn.add_to_team(Teams.ARCH_MOTHER), tobias.add_to_team(Teams.HIDDEN_KING), gimli.add_to_team(Teams.HIDDEN_KING), gandalf.add_to_team(Teams.HIDDEN_KING), frodo.add_to_team(Teams.HIDDEN_KING),
+    #User.check_team_characters(Teams.ARCH_MOTHER),  User.check_team_characters(Teams.HIDDEN_KING)
+
+    #print(sivert.character.characterType)
+    #print(sivert.character.typeWeights[ItemType.SUSTAIN])
+
+    sivert.is_fed_calc()
+    sivert.simulate_game()
--- User.py
+++ User.py
@@ -1,7 +1,9 @@
 ﻿from enum import Enum
-
+from typing import Union, Tuple, Dict, Any
+import App
+from App import ItemType
 from App import Character
-
+"""""
 class Teams(Enum):
     ARCH_MOTHER = 1
     HIDDEN_KING = 2
@@ -19,9 +21,7 @@ class User:
         self.deaths = deaths
         self.assists = assists
 
-
-    def character_score_calculator(self):
-        return
+#TODO legg til team i init og sjekk at det er gyldig, og legg til i teamet der, slipper å kalle add_to_team etterpå flere ganger
 
     def add_to_team(self, team: Teams) -> None:
         if team == Teams.ARCH_MOTHER:
@@ -41,6 +41,9 @@ class User:
                 print("Added to team Hidden King:", self.character.name)
             else:
                 raise ValueError("Invalid team name. Must be 'Arch Mother' or 'Hidden King'.")
+        else:
+            print(f"{self.character.name} is already in a team. Cannot add to another team OR be added again.")
+
     def check_team_membership(self) -> Teams:
         if self in User.Arch_Mother:
             return Teams.ARCH_MOTHER
@@ -48,7 +51,19 @@ class User:
             return Teams.HIDDEN_KING
         else:
             raise ValueError("Not part off any team.")
+
+    def add_weight(self, itemType, updated_value, debug = False) -> None:
+        old = self.character.typeWeights.get()
+        print(f"Old weight for {itemType}: {old}")
+        if old is None:
+            print(f"[WARN] Missing key in typeWeights for {itemType} \-> creating with0.0")
+            old = 0.0
+        self.character.typeWeights[itemType] = old + updated_value
+        if debug:
+            print(f"Updated {itemType}: {old} \-> {self.character.typeWeights[itemType]}")
+
     def is_fed_calc(self, debug = False) -> None:
+        global ctype
         selfkda = (self.kills + (self.assists / 2)) / max(1, self.deaths)  # Avoids division by zero
         team_check = self.check_team_membership()
         team_variable = User.Arch_Mother if team_check == Teams.ARCH_MOTHER else User.Hidden_King
@@ -62,16 +77,41 @@ class User:
             user_kda = (user.kills + (user.assists / 2)) / max(1, user.deaths)
             total_kda += user_kda
             team_avg_kda = total_kda / len(teammates)
-        if debug == True:
-            print(f"Total KDA: {total_kda}")
-            print(f"Team Average KDA: {team_avg_kda}")
-            print(f"{self.character.name}'s KDA: {selfkda}")
+
+        if debug:
+            print(f"Total KDA / All teammate KDA added upp: {total_kda:.2f}")
+            print(f"Team Average KDA: {team_avg_kda:.2f}")
+            print(f"{self.character.name}'s KDA: {selfkda:.2f}")
+            print(f"Before Type Weights: {self.character.typeWeights}")
+
+        ctype = self.character.characterType
+        ctype_name = getattr(ctype, "name", str(ctype)).upper()
 
         if selfkda > team_avg_kda:
-            print(f"{self.character.name} is fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
-        elif selfkda < team_avg_kda:
-            print(f"{self.character.name} is not fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
+            if debug:
+                print("got past the selfkda > team_avg_kda check")
+            if ctype_name == "SUPPORT":
+                if debug:
+                    print(f"{self.character.name} is a support, so they get more sustain items.")
+                self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+            elif ctype_name == "TANK":
+                if debug:
+                    print(f"{self.character.name} is a tank, so they get more sustain and gun items.")
+                self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+                self.add_weight(ItemType.GUN, 0.4, debug=debug)
+            elif ctype_name == "DPS":
+                if debug:
+                    print(f"{self.character.name} is a DPS, so they get more gun and spirit items.")
+                self.add_weight(ItemType.GUN, 0.4, debug=debug)
 
+            print(
+                f"{self.character.name} is fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
+        elif selfkda < team_avg_kda:
+            self.add_weight(ItemType.SUSTAIN, 0.4, debug=debug)
+            print(
+                f"{self.character.name} is not fed with a KDA of {selfkda:.2f} compared to the team average of {team_avg_kda:.2f}.")
+        if debug:
+            print(f"After Type Weights: {self.character.typeWeights}")
     @classmethod
     def check_team_characters(cls, team: Teams) -> None:
         if team == Teams.ARCH_MOTHER:
@@ -83,3 +123,4 @@ class User:
         else:
             raise ValueError("Invalid team name. Must be 'Arch Mother' or 'Hidden King'.")
 
+"""""
